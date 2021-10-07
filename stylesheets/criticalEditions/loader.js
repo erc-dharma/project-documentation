1adc3be08742aee751e24b6b8216d7e3e175d08e
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-10-07 15:49:15 +0200

    cleaning the code and commenting it for documentation purposes

--- stylesheets/criticalEditions/loader.js
+++ stylesheets/criticalEditions/loader.js
@@ -57,39 +57,15 @@ $('.move-to-right').on({
 });
 
 /* Trying to add the lac. $siglum between lacunaStart and lacunaEnd 
-$( ".lacunaStart" )
-  .contents()
-  .filter(function(){
-    return this.nodeType !== 1;
-  })
-  .wrap( "<b></b>" );
+Code only for the lateral tooltip*/
 
-$(document).ready(function(){
-    var newHeading = "<h2>Important Note:</h2>";
-    var newParagraph = document.createElement("p");
-    newParagraph.innerHTML = "<em>Lorem Ipsum is dummy text...</em>";
-    var newImage = $('<img src="images/smiley.png" alt="Symbol">');
-    $("p").before(newHeading, newParagraph, newImage);
-});*/
-
-/*
-    /\* wrapping the newRdg and the siglum part if the reading line into the main span container *\/
-    $(newRdg, siglum).wrap('<span class="reading-line"></span>'); 
-});*/
-
-
-$(document).ready(function() {
-    /* newRDG create the lac. textual content always the same so added as such directly */
     $( ".lacunaStart" ).each(function() {
+    /* newRDG create the lac. textual content always the same so added as such directly */
         var newRdg =$('<br/><span class="translit LatnLatn"><span class="font-italic" style="color:black;">lac. </span></span>')
         /* Selectionne lacunaStartSiglum et cherche le premier element suivant avec la class siglum, récupere son contenu textuel - nodetype 3 s"assure qu'il s'agit bien d'une chaîne de caractère  */
-        var siglum= $(this).find(".siglum:first").contents().clone().wrap( '<span class="font-weight-bold "><a class="siglum" href="#"></a></span>').filter(function(){
+        var siglum= $(this).find(".siglum:first").contents().clone().filter(function(){
                 return this.nodeType === 3;})
-        /*var wrappedSiglum = $(siglum).wrap( '<span class="font-weight-bold "><a class="siglum" href="#"></a></span>' )*/
-    /* wrap the siglum textual content into its html structure !!! CAREFUL about the integration of the the siglum into the @href with $(this) and concat:
-     * .wrap( '<span class="font-weight-bold "><a class="siglum" href="#"></a></span>' ) */
-        /*$(this).parents('.popover-content').nextAll('.popover-content').find('.reading-line:last').after().append(newRdg, siglum);*/
-               $(this).parents('.popover-content').nextUntil('.popover-content:has(.lacunaEnd)').find('.reading-line:last').after().append(newRdg, siglum.wrap( '<a class="siglum" href="#"></a>').parent().wrap( '<span class="font-weight-bold "></span>').parent());
         
+    /* move to the parent span element, then take all the following span elements until one has a descendant with the class lacunaEnd - find class .reading-line and after it add the newRDG and the siglum wrapped in html elements */
+            $(this).parents('.popover-content').nextUntil('.popover-content:has(.lacunaEnd)').find('.reading-line:last').after().append(newRdg, siglum.wrap( '<a class="siglum" href="#"></a>').parent().wrap( '<span class="font-weight-bold "></span>').parent());
     }); 
-});
