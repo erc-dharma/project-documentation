e004d1c3e91910b22d63991f107b39dadb514829
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-11-17 15:34:46 +0100

    distinguishing the loader from diplEd and critEd

---
+++ stylesheets/diplomaticEditions/loader-diplEd.js
@@ -0,0 +1,19 @@
+var content = $("#root").append(content);
+            initializePopovers();
+                     
+function initializePopovers() {  
+            $('[data-toggle="popover"]').popover({
+            html : true,
+            container: 'body',
+            trigger : 'hover',
+            content: function() {
+            var idtarget = $(this).attr("data-target");
+            console.log($('#'+idtarget));
+            return $('#'+idtarget).html();
+            }
+            });
+};
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
