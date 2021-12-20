3d876c93fb8663f2716c8714efd69d76e0237a9d
ajaniak <33328906+ajaniak@users.noreply.github.com>
2021-12-20 15:51:12 +0100

    updating the loader for dipl Ed

--- stylesheets/diplomaticEditions/loader-diplEd.js
+++ stylesheets/diplomaticEditions/loader-diplEd.js
@@ -15,5 +15,30 @@ function initializePopovers() {
 };
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+      $("#sidebar-wrapper").mCustomScrollbar({
+         theme: "minimal"
+    });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar-wrapper').toggleClass('active');
+    });
+  $('.lg-omitted .l:first-child, .omitted').prepend('⟨');
+$('.lg-omitted .l:last-child, .omitted').append('⟩');
+$('.explanation, .unclear').prepend('(');
+$('.explanation, .unclear').append(')');
+$('.subaudible, .lineation, .foliation, .lost-illegible, .gap').prepend('[');
+$('.subaudible, .lineation, .foliation, .lost-illegible, .gap').append(']');
+$('.hyphenfront, .hyphenaround').prepend('-');
+$('.hyphenback, .hyphenaround').append('-');
+$('.circlefront, .circlearound').prepend('°');
+$('.circleback, .circlearound').append('°');
+$('.surplus').prepend('{');
+$('.surplus').append('}');
+$('.ed-insertion').prepend('⟨⟨');
+$('.ed-insertion').append('⟩⟩');
+$('.scribe-deletion').prepend('⟦');
+$('.scribe-deletion').append('⟧');
+$('.sic-crux').prepend('†');
+$('.sic-crux').append('†');
 })
+
