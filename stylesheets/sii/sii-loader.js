d0074702237b129c4f806f154b176891972d6c06
ajaniak <33328906+ajaniak@users.noreply.github.com>
2022-06-29 10:43:25 +0200

    adding a first display and loader

---
+++ stylesheets/sii/sii-loader.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#sidebar-wrapper").mCustomScrollbar({
+         theme: "minimal"
+    });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar-wrapper').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+})
