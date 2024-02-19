e46dd067311b4677aa5c49f37512999d5c692040
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-02-19 13:45:26 +0100

    Drop unused files

--- bibliography/pyzotero/DHARMA_st2tags_v01.py
+++
@@ -1,9 +0,0 @@
-# Written by Axelle Janiak for DHARMA - march 2020
-from pyzotero import zotero
-zot = zotero.Zotero('1633743', 'group', 'SNTOlhPbtO3n21oq1Son78B1')
-items = zot.everything(zot.top()) #collection Tamil: items = zot.everything(zot.collection_items('NYLTL87Y'))
-# Chercher les shortTitles
-for item in items:
-    newtag1 = item['data'].get('shortTitle')
-    zot.add_tags(item, newtag1)
-    print(item['data']['key'], '+', newtag1)
