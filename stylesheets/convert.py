5e381e300beb071474610809473a14145e121f80
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-03-12 14:16:09 +0100

    Transform script adjustments for Windows

--- stylesheets/convert.py
+++ stylesheets/convert.py
@@ -4,11 +4,11 @@ import os, sys, platform, tempfile, subprocess
 
 system = platform.system()
 if system == "Windows":
-	browser = "start"
+	browser, shell = "start", True
 elif system == "Darwin":
-	browser = "open"
+	browser, shell = "open", False
 else:
-	browser = "xdg-open"
+	browser, shell = "xdg-open", False
 
 if len(sys.argv) != 2:
 	print(f"Usage: {sys.argv[0]} FILE", file=sys.stderr)
@@ -19,4 +19,4 @@ tmp_dir = tempfile.gettempdir()
 output = os.path.join(tmp_dir, "dharma_output.html")
 
 subprocess.run(["curl", "-s", "-F", f"file=@{input}", "-o", output, "https://dharmalekha.info/convert"], check=True)
-subprocess.run([browser, output])
+subprocess.run([browser, output], shell=shell)
