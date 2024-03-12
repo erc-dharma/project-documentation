a9c3e99b531840c94c8aee31612f3f7435bae156
Michaël Meyer <michaelnm.meyer@gmail.com>
2024-03-12 13:39:56 +0100

    Conversion script

--- stylesheets/convert.py
+++ stylesheets/convert.py
@@ -4,7 +4,7 @@ import os, sys, platform, tempfile, subprocess
 
 system = platform.system()
 if system == "Windows":
-	browser = "explorer.exe"
+	browser = "start"
 elif system == "Darwin":
 	browser = "open"
 else:
@@ -19,4 +19,4 @@ tmp_dir = tempfile.gettempdir()
 output = os.path.join(tmp_dir, "dharma_output.html")
 
 subprocess.run(["curl", "-s", "-F", f"file=@{input}", "-o", output, "https://dharmalekha.info/convert"], check=True)
-subprocess.run([browser, output], check=True)
+subprocess.run([browser, output])
